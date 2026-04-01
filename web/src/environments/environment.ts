e12877a633912791a55afaf2fb38e07cb14c47a7
jwgiblin3 <jwgiblin3@gmail.com>
2026-04-01 18:52:54 -0400

    Env

--- web/src/environments/environment.ts
+++ web/src/environments/environment.ts
@@ -1,7 +1,9 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:59606/api',
+    apiUrl: 'https://teetime-automation-production.up.railway.app/api',
   googleClientId: '219406115199-7rvk4beqnqhs8u0b1r7dkb3o7s8hif54.apps.googleusercontent.com'
+ // apiUrl: 'http://localhost:59606/api',
+ // googleClientId: '219406115199-7rvk4beqnqhs8u0b1r7dkb3o7s8hif54.apps.googleusercontent.com'
 };
 
 

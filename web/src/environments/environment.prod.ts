8f9072c3f1d3333e92e948d3d561beb22765d57d
jwgiblin3 <jwgiblin3@gmail.com>
2026-04-01 17:45:51 -0400

    testing

--- web/src/environments/environment.prod.ts
+++ web/src/environments/environment.prod.ts
@@ -1,5 +1,7 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://teetime-automation-production.up.railway.app/api',
-    googleClientId: '219406115199-7rvk4beqnqhs8u0b1r7dkb3o7s8hif54.apps.googleusercontent.com'
+    apiUrl: 'http://localhost:59606/api',
+  googleClientId: '219406115199-7rvk4beqnqhs8u0b1r7dkb3o7s8hif54.apps.googleusercontent.com'
+ // apiUrl: 'https://teetime-automation-production.up.railway.app/api',
+  //  googleClientId: '219406115199-7rvk4beqnqhs8u0b1r7dkb3o7s8hif54.apps.googleusercontent.com'
 };
